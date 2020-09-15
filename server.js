924cbf508d5d188adcf7cb05793654d8bc7a7227
shubpy <shubpy@gmail.com>
2020-09-16 01:29:43 +0530

    New changes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// load the things we need
+/ load the things we need
 var express = require('express');
 var app = express();
 
@@ -13,11 +13,57 @@ app.get('/', function(req, res) {
 });
 
 
+
 app.use(express.static(__dirname + '/public'))
 
 // about page 
 app.get('/about', function(req, res) {
 	res.render('pages/about');
+});
+
+// services page 
+app.get('/services', function(req, res) {
+	res.render('pages/services');
+});
+
+// pricing page 
+app.get('/pricing', function(req, res) {
+	res.render('pages/pricing');
+});
+
+// portfolio page 
+app.get('/portfolio', function(req, res) {
+	res.render('pages/portfolio');
+});
+
+// contact page 
+app.get('/contact', function(req, res) {
+	res.render('pages/contact');
+});
+
+//Careers page 
+app.get('/careers', function(req, res) {
+	res.render('pages/careers');
+});
+
+//miscellaneous page 
+app.get('/miscellaneous', function(req, res) {
+	res.render('pages/miscellaneous');
+});
+
+//first page 
+app.get('/first', function(req, res) {
+	res.render('pages/first');
+});
+
+//second page 
+app.get('/second', function(req, res) {
+	res.render('pages/second');
+});
+
+//fourth page 
+app.get('/fourth', function(req, res) {
+	res.render('pages/fourth');
 });
 
 app.listen(process.env.PORT || 5000);
