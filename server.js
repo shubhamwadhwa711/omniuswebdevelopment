f616b19502b6bba075d1b9aecccdcd09a0358b97
shubpy <shubpy@gmail.com>
2020-09-24 22:16:25 +0530

    New changes.

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.set('view engine', 'ejs');
 
 // index page 
 app.get('/', function(req, res) {
-	res.render('pages/index');
+	//res.render('pages/index');
+	res.render('pages/index', { title: 'Omnius Web Development' });
 });
 
 
@@ -18,52 +19,52 @@ app.use(express.static(__dirname + '/public'))
 
 // about page 
 app.get('/about', function(req, res) {
-	res.render('pages/about');
+	res.render('pages/about', { title: 'Omnius Web Development - About' });
 });
 
 // services page 
 app.get('/services', function(req, res) {
-	res.render('pages/services');
+	res.render('pages/services', { title: 'Omnius Web Development - Services' });
 });
 
 // pricing page 
 app.get('/pricing', function(req, res) {
-	res.render('pages/pricing');
+	res.render('pages/pricing' , { title: 'Omnius Web Development - Pricing' });
 });
 
 // portfolio page 
 app.get('/portfolio', function(req, res) {
-	res.render('pages/portfolio');
+	res.render('pages/portfolio', { title: 'Omnius Web Development - Portfolio' });
 });
 
 // contact page 
 app.get('/contact', function(req, res) {
-	res.render('pages/contact');
+	res.render('pages/contact', { title: 'Omnius Web Development - Contact' });
 });
 
 //Careers page 
 app.get('/careers', function(req, res) {
-	res.render('pages/careers');
+	res.render('pages/careers' , { title: 'Omnius Web Development - Careers' });
 });
 
 //miscellaneous page 
 app.get('/miscellaneous', function(req, res) {
-	res.render('pages/miscellaneous');
+	res.render('pages/miscellaneous', { title: 'Omnius Web Development - Miscellaneous' });
 });
 
 //first page 
 app.get('/first', function(req, res) {
-	res.render('pages/first');
+	res.render('pages/first', { title: 'Omnius Web Development - First' });
 });
 
 //second page 
 app.get('/second', function(req, res) {
-	res.render('pages/second');
+	res.render('pages/second' , { title: 'Omnius Web Development - Second' });
 });
 
 //fourth page 
 app.get('/fourth', function(req, res) {
-	res.render('pages/fourth');
+	res.render('pages/fourth', { title: 'Omnius Web Development - Fourth' });
 });
 
 app.listen(process.env.PORT || 5000);
